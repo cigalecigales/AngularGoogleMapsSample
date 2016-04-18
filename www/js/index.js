54e668b9fb27538fab6d696f339ba3694b6d52b6
cigalecigales <cigalecigales@gmail.com>
2016-04-18 10:23:39 +0900

    Ver1.0.2 Display googlemap

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,22 @@
 var app = ons.bootstrap('AngularGoogleMapsSample', ['onsen', 'uiGmapgoogle-maps']);
 
-app.controller("TopPageController", ['$scope', function($scope){
-	
+app.config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        v: '3.22'
+    });
+}]);
+
+app.controller("TopPageController", ['$scope', 'uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.then(function(maps) {
+        $scope.map = {
+            center: {
+                latitude: 35.681382,
+                longitude: 139.766084
+            },
+            zoom: 12,
+            options: {
+                mapTypeId: google.maps.MapTypeId.HYBRID
+            }
+        };
+    });
 }]);
