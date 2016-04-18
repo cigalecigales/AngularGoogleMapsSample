9b8bbc9bc9f7c6a9d8ddf1835d635a09a52ab264
cigalecigales <cigalecigales@gmail.com>
2016-04-18 12:53:10 +0900

    Ver1.0.3 Add directions settings

--- www/js/index.js
+++ www/js/index.js
@@ -9,6 +9,7 @@ app.config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
 app.controller("TopPageController", ['$scope', 'uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi) {
     uiGmapGoogleMapApi.then(function(maps) {
         $scope.map = {
+            control: {},
             center: {
                 latitude: 35.681382,
                 longitude: 139.766084
@@ -16,7 +17,44 @@ app.controller("TopPageController", ['$scope', 'uiGmapGoogleMapApi', function($s
             zoom: 12,
             options: {
                 mapTypeId: google.maps.MapTypeId.HYBRID
-            }
+            },
+            markers: [{
+                id: 1,
+                latitude: 35.681382,
+                longitude: 139.766084
+            }, {
+                id: 2,
+                latitude: 35.628856,
+                longitude: 139.738854
+            }]
         };
+
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        var directionsService = new google.maps.DirectionsService();
+        var geocoder = new google.maps.Geocoder();
+
+        $scope.directions = {
+            origin: "Collins St, Melbourne, Australia",
+            destination: "MCG Melbourne, Australia",
+            showList: false
+        }
+
+        $scope.getDirections = function() {
+            var request = {
+                origin: $scope.directions.origin,
+                destination: $scope.directions.destination,
+                travelMode: google.maps.DirectionsTravelMode.DRIVING
+            };
+            directionsService.route(request, function(response, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                    directionsDisplay.setMap($scope.map.control.getGMap());
+                    directionsDisplay.setPanel(document.getElementById('directionsList'));
+                    $scope.directions.showList = true;
+                } else {
+                    alert('Google route unsuccesfull!');
+                }
+            });
+        }
     });
 }]);
