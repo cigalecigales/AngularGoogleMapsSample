b18a99f6043b6ad7eccf6fecf46264bf64dee94c
cigalecigales <cigalecigales@gmail.com>
2016-05-19 13:02:40 +0900

    ルート検索ブランチ追加

--- www/js/index.js
+++ www/js/index.js
@@ -29,17 +29,22 @@ app.controller("TopPageController", ['$scope', 'uiGmapGoogleMapApi', function($s
             }]
         };
 
+        // ルート情報の表示サービス
         var directionsDisplay = new google.maps.DirectionsRenderer();
+        // ルート検索サービス
         var directionsService = new google.maps.DirectionsService();
-        var geocoder = new google.maps.Geocoder();
 
         $scope.directions = {
+            // FROM
             origin: "Collins St, Melbourne, Australia",
+            // TO
             destination: "MCG Melbourne, Australia",
             showList: false
         }
 
+        // ルート取得処理
         $scope.getDirections = function() {
+            // ルート検索のモードを取得
             var travelModeList = document.getElementsByName("travelMode");
             var travelMode = "DRIVING";
             for (var i = 0; i < travelModeList.length; i++) {
@@ -47,14 +52,16 @@ app.controller("TopPageController", ['$scope', 'uiGmapGoogleMapApi', function($s
                     travelMode = travelModeList[i].value;
                 }
             }
-
+            // リクエストを作成
             var request = {
-                origin: $scope.directions.origin,
-                destination: $scope.directions.destination,
-                travelMode: google.maps.DirectionsTravelMode[travelMode]
+                origin: $scope.directions.origin, // FROM
+                destination: $scope.directions.destination, // TO
+                travelMode: google.maps.DirectionsTravelMode[travelMode] // モード
             };
+            //ルート検索サービスの呼び出し
             directionsService.route(request, function(response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
+                  // 画面にルート情報を表示
                     directionsDisplay.setDirections(response);
                     directionsDisplay.setMap($scope.map.control.getGMap());
                     directionsDisplay.setPanel(document.getElementById('directionsList'));
